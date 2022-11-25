9001a2020119120f770731c891ab476671468564
Mariya <m_relaxeena@mail.ru>
2022-11-25 18:00:20 +0400

    дз

---
+++ Homework2/task13/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(1,1000);
+Console.WriteLine();
+if  ( (number / 100) > 0)
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является {(number / 100) % 10}");
+    }
+else Console.WriteLine($"У числа {number} нет третьей цифры");
