43d780cd9ea6e4f98f37943971aeaa627ef27e57
Mariya <m_relaxeena@mail.ru>
2022-11-28 17:26:28 +0400

    task15

---
+++ Homework2/task 15/Program.cs
@@ -0,0 +1,9 @@
+﻿int dayNumber = new Random().Next(1, 8);
+string[] days ={ "понедельник", "вторник", "среда", "четверг", "пятница","суббота", "воскресенье" };
+string txtResult = "";
+if ( dayNumber > 5)
+{
+    txtResult = $" - выходной день";
+}
+else txtResult = $" - рабочий день";
+Console.WriteLine($"{dayNumber} день недели - это {days[dayNumber-1]}{txtResult}");
